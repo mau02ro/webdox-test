0b8d4ad534b87f2e191bee1d5df6e8ddcbeecab7
Mauro Hernández <mauroDeveloper@hotmail.com>
2021-07-28 21:31:51 -0400

    Bug resize

--- header/header.js
+++ header/header.js
@@ -2,26 +2,28 @@ const landing_header = document.getElementById("landing_header");
 const header_openMenu = document.getElementById("header_openMenu");
 const header_container = document.getElementById("header_container");
 
-const CONTROLLER_CLASS = "open_menu";
+const CLASS_OPEN = "open_menu";
+const CLASS_CLOSE = "close_menu";
 
 function header_menuController(){
-  if(!landing_header.classList.contains(CONTROLLER_CLASS)){
-    landing_header.classList.add(CONTROLLER_CLASS);
+  if(!landing_header.classList.contains(CLASS_OPEN)){
+    landing_header.classList.add(CLASS_OPEN);
     document.getElementsByTagName("body")[0].style.overflow = "hidden";
     document.getElementsByTagName("body")[0].style.position = "fixed";
   }else{
-    landing_header.classList.add("close_menu");
-    header_container.addEventListener("animationend", header_closeMenu)
+    landing_header.classList.add(CLASS_CLOSE);
+    header_container.addEventListener("animationend", header_closeMenu);
   }
 }
 
 function header_closeMenu(){
-  landing_header.classList.remove(CONTROLLER_CLASS);
-  landing_header.classList.remove("close_menu");
+  landing_header.classList.remove(CLASS_OPEN);
+  landing_header.classList.remove(CLASS_CLOSE);
 
   header_container.removeEventListener("animationend", header_closeMenu);
   document.getElementsByTagName("body")[0].style.overflow = "initial";
   document.getElementsByTagName("body")[0].style.position = "initial";
 }
 
-header_openMenu.addEventListener("click", header_menuController)
+header_openMenu.addEventListener("click", header_menuController);
+window.addEventListener('resize', header_closeMenu);
