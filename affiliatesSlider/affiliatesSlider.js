299cbfddc5bdf31030d3291b2fd540ceaffea798
Mauro Hernández <mauroDeveloper@hotmail.com>
2021-07-29 20:56:22 -0400

    FIx bug

--- affiliatesSlider/affiliatesSlider.js
+++ affiliatesSlider/affiliatesSlider.js
@@ -1,17 +1,22 @@
 const slider = document.getElementById("affiliatesSlider");
 const slider_container = document.getElementById("affiliatesSlider_container");
 
-// Settings define slider behavior.
 const CONFIG_SLIDER = {
-  num_show: 6, // Tndicates how many slides will be displayed.
-  time: 3000, // Slider speed(ms).
+  num_show: 6,
+  time: 3000,
+  breakingPoints: [
+    {
+      size: 768,
+      num_show: 4,
+    },
+    {
+      size: 425,
+      num_show: 3,
+    }
+  ]
 }
 
-// -------------------------------------------------------------------
-// Utilities to manipulate the slider.
 function Utilities(state){
-
-  // Set the width of each item on the slider.
   this.setWidth = () => {
     let items_slide = document.querySelectorAll(".slide");
     let width_slider = slider.getBoundingClientRect().width;
@@ -24,13 +29,6 @@ function Utilities(state){
     })
   }
 
-  /*
-  Due to the operation of the slider, we have to clone a number of items,
-  these functions respectively are in charge of handling this functionality.
-  
-    - cloneSlide: Clone the slides.
-    - removeClones: Remove cloned items.
-  */
   this.cloneSlide = () => {
     let items_slide = document.querySelectorAll(".slide");
 
@@ -51,7 +49,6 @@ function Utilities(state){
     }
   }
 
-  // Calculates slider skip or shift.
   this.calculateJump = () => {
     let position_slider = slider_container.getBoundingClientRect().x;  
     let offset_size = position_slider;
@@ -65,28 +62,19 @@ function Utilities(state){
     return position_slider - (offset_size + state.jump_space);
   }
 
-  // This method moves the slider.
   this.moveSlider = (jump) => {
     slider_container.style.transform = `translateX(${jump}px)`;
   }
 
-  // Set the speed of slider transitions.
   this.setTransition = (indicator) => {
     if(indicator){
-      slider_container.style.transition = `transform ${CONFIG_SLIDER.time/2}s`;
+      slider_container.style.transition = "transform 2s";
     }else{
       slider_container.style.transition = "transform 0s";
     }
   }
 }
 
-// -------------------------------------------------------------------
-// "setInterval" controls.
-/*
-This function is in charge of managing the start, stop and restart of
-the interval, the function that is received as a parameter, the one
-that will be executed in each interval.
-*/
 function ControllerInterval(fn){
   let timer = null;
 
@@ -111,44 +99,37 @@ function ControllerInterval(fn){
   }
 }
 
-// -------------------------------------------------------------------
-// This function is the main controller of the slider.
 function ControllerSlider(){
-  // Local state.
   const state = {
-    margin: slider_container.getBoundingClientRect().x, // Design margin (window - slider).
-    hop_counter: 0, // Hop counter.
-    jump_space: 0, // Jump space.
+    margin: slider_container.getBoundingClientRect().x, // Design margin (window - slider)
+    hop_counter: 0, // Hop counter
+    jump_space: 0, // Jump space
     initNum_slide: slider_container.children.length, // Initial number of slide,
-    num_show: CONFIG_SLIDER.num_show, // Tndicates how many slides will be displayed.
+    num_show: CONFIG_SLIDER.num_show
   }
 
-  // Interval Driver Instances and Utility Driver Instances.
   const Utility = new Utilities(state);
+
   let Interval = new ControllerInterval(() => {
     this.controller()
   });
 
-
-  // Start the slider.
   this.start = () => {
     this.responsive()
     Utility.cloneSlide();
     Interval.start();
   }
 
-  // Reset the slider.
   this.reset = () => {
     state.hop_counter = 0;
     Utility.setTransition(false);
     Utility.moveSlider(0);
 
-    slider_container.removeEventListener("transitionend", this.reset);
+    slider_container.removeEventListener("transitionend", this.reset)
   }
 
-  // Reboot the slider.
   this.reboot = () => {
-    this.responsive();
+    this.responsive()
 
     Utility.setTransition(false);
 
@@ -163,7 +144,6 @@ function ControllerSlider(){
     Interval = Interval.reset();
   }
 
-  // Principal function.
   this.controller = () => {
     Utility.setTransition(true);
 
@@ -173,12 +153,10 @@ function ControllerSlider(){
     state.hop_counter += 1;
 
     if(state.hop_counter == state.initNum_slide){
-      // When the transaction finished, the restart function is executed.
-      slider_container.addEventListener("transitionend", this.reset);
+      slider_container.addEventListener("transitionend", this.reset)
     }
   }
 
-  // Indicates the number of slides to be displayed, it is used in the answer.
   this.responsive = () => {
     let { width } = document.getElementsByTagName("body")[0].getBoundingClientRect();
     let val = CONFIG_SLIDER.num_show;
@@ -193,23 +171,20 @@ function ControllerSlider(){
 
     state.num_show = val;
   }
+
+  this.test = () => {
+    Utility.cloneSlide();
+    Utility.removeClones();
+  }
 }
 
-// IIFE is used to keep the slider instance in a closed scope.
 (() => {
   const AffiliatesSlider = new ControllerSlider();
-  let last_width = document.getElementsByTagName("body")[0].getBoundingClientRect().width;
 
-  // Start the slider.
-  AffiliatesSlider.start();
-
-  // Used by resize event to calculate slider views.
+  window.addEventListener("load", () => {
+    AffiliatesSlider.start();
+  })
   window.addEventListener("resize", () => {
-    let { width } = document.getElementsByTagName("body")[0].getBoundingClientRect().width;
-
-    if(width !== last_width){
-      AffiliatesSlider.reboot();
-      last_width = width;
-    }
+    AffiliatesSlider.reboot(3);
   })
 })()
